7251369787cd53f8b8902bf403146233bd733d89
bruno <A01367102@itesm.mx>
2021-10-29 12:26:32 -0500

    Update main.py

--- Programas/main.py
+++ Programas/main.py
@@ -18,12 +18,14 @@ k2 = emboss()
 
 k3 = sharpen()
 
+k4 = passf()
 
 
 J0 = ndimage.convolve(I, k0, mode='constant', cval=0.0)
 J1 = ndimage.convolve(I, k1, mode='constant', cval=0.0)
 J2 = ndimage.convolve(I, k2, mode='constant', cval=0.0)
 J3 = ndimage.convolve(I, k3, mode='constant', cval=0.0)
+J4 = ndimage.convolve(I, k4, mode='constant', cval=0.0)
 
 
 plt.figure(figsize = (15,15))
@@ -40,13 +42,17 @@ plt.subplot(3,3,3)
 plt.imshow(J1)
 plt.xlabel('Sobel Edge Vertical')
 
-plt.subplot(3,3,4)
+plt.subplot(3,4,4)
 plt.imshow(J2)
 plt.xlabel('Emboss Filter')
 
-plt.subplot(3,3,5)
+plt.subplot(3,4,5)
 plt.imshow(J3)
 plt.xlabel('Sharpen Image')
+
+plt.subplot(3,4,6)
+plt.imshow(J3)
+plt.xlabel('Pass Filter')
 
 
 plt.grid(False)
