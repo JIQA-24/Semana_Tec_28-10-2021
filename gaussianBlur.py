025653c8480f77da6b760f70b6167cd369f3c1b4
JIQA-24 <A01366686@itesm.mx>
2021-10-29 12:00:43 -0500

    updated gaussian_blur

--- gaussianBlur.py
+++ gaussianBlur.py
@@ -3,58 +3,54 @@ import numpy
 from PIL import Image
 from scipy import ndimage
 
-Is = Image.open('ocardo.jpg');
-I = Is.convert('L');
-I = numpy.asarray(I);
-I = I / 255.0;
+Is = Image.open('capi.jpg'); # imagen del sudo
+I = Is.convert('L'); # se convierte a escala de grises
+I = numpy.asarray(I); # conversion numerica para poder operar de 0-1
+I = I / 255.0; # normalizacion 0 - 1
+
+
+
+def pad_with(vector, pad_width, iaxis, kwargs):
+    pad_value = kwargs.get('padder', 10)
+    vector[:pad_width[0]] = pad_value
+    vector[-pad_width[1]:] = pad_value
+
+I = numpy.pad(I, 10, pad_with, padder=0)
+
+M = kernel(sigma, alpha, I, )
+
+kM = numpy.array(M)
 
 k0 = numpy.array([[0,-1,0],[-1,0,1],[0,1,0]])
-
-'''
-1 -1 1
--1 4 -1
- 1 -1 1
-'''
-
 k1 = numpy.array([[0,0,0],[-1,0,1],[0,0,0]])
-
-'''
- 0 0 0
--1 0 1
- 0 0 0
-'''
-
-
 k2 = numpy.array([[0,-1,0],[0,0,0],[0,1,0]])
-
-'''
- 0 -1 0
- 0  0 0
- 0  1 0
-'''
 
 J0 = ndimage.convolve(I, k0, mode='constant', cval=0.0)
 J1 = ndimage.convolve(I, k1, mode='constant', cval=0.0)
 J2 = ndimage.convolve(I, k2, mode='constant', cval=0.0)
 
-plt.figure(figsize = (15,15))
+plt.figure(figsize = (20,20))
 
-plt.subplot(2,2,1)
+plt.subplot(3,3,1)
 plt.imshow(Is)
 plt.xlabel('Input Image')
 
-plt.subplot(2,2,2)
+plt.subplot(3,3,2)
 plt.imshow(J0)
 plt.xlabel('VH direction')
 
-plt.subplot(2,2,3)
+plt.subplot(3,3,3)
 plt.imshow(J1)
 plt.xlabel('Horizontal direction')
 
-plt.subplot(2,2,4)
+plt.subplot(3,3,4)
 plt.imshow(J2)
 plt.xlabel('Vertical direction')
 
+plt.subplot(3,3,5)
+plt.imshow(J2)
+plt.xlabel('Este es reptido Vertical direction')
 
-plt.grid(False)
+
+#plt.grid(False)
 plt.show()
