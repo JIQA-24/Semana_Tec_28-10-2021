08c1f6a55452e3b5b4f1a2d87fd3383ce5ec9e73
JIQA-24 <A01366686@itesm.mx>
2021-10-29 11:23:34 -0500

    convolution

--- gaussianBlur.py
+++ gaussianBlur.py
@@ -1,13 +1,60 @@
-import cv2
-import numpy as np
-from matplotlib import pyplot as plt
- 
-img = cv2.imread('capi.jpg')
- 
-blur = cv2.blur(img,(3,3))
- 
-plt.subplot(121),plt.imshow(img),plt.title('Original')
-plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(blur),plt.title('Difuminada')
-plt.xticks([]), plt.yticks([])
+import matplotlib.pyplot as plt
+import numpy
+from PIL import Image
+from scipy import ndimage
+
+Is = Image.open('ocardo.jpg');
+I = Is.convert('L');
+I = numpy.asarray(I);
+I = I / 255.0;
+
+k0 = numpy.array([[0,-1,0],[-1,0,1],[0,1,0]])
+
+'''
+1 -1 1
+-1 4 -1
+ 1 -1 1
+'''
+
+k1 = numpy.array([[0,0,0],[-1,0,1],[0,0,0]])
+
+'''
+ 0 0 0
+-1 0 1
+ 0 0 0
+'''
+
+
+k2 = numpy.array([[0,-1,0],[0,0,0],[0,1,0]])
+
+'''
+ 0 -1 0
+ 0  0 0
+ 0  1 0
+'''
+
+J0 = ndimage.convolve(I, k0, mode='constant', cval=0.0)
+J1 = ndimage.convolve(I, k1, mode='constant', cval=0.0)
+J2 = ndimage.convolve(I, k2, mode='constant', cval=0.0)
+
+plt.figure(figsize = (15,15))
+
+plt.subplot(2,2,1)
+plt.imshow(Is)
+plt.xlabel('Input Image')
+
+plt.subplot(2,2,2)
+plt.imshow(J0)
+plt.xlabel('VH direction')
+
+plt.subplot(2,2,3)
+plt.imshow(J1)
+plt.xlabel('Horizontal direction')
+
+plt.subplot(2,2,4)
+plt.imshow(J2)
+plt.xlabel('Vertical direction')
+
+
+plt.grid(False)
 plt.show()
