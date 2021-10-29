4cda83974eb9097320e9af85daffc9a9624ea78e
JIQA-24 <A01366686@itesm.mx>
2021-10-29 10:38:34 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
-#son las 10
-def MexHat(sigma, K):
-    M =  numpy.zeros((K,K));
-    for x in range(0,5):
-        for y in range(0,K):
-            M[x,y] = 1/(pi*sigma**2) * ( 1-1-2*(x**2+y**2)/sigma**2) * numpy.exp(- (x**2+y**2)/(2*sigma**2))
-
-MexHat(sigma = 1, K = 5)
-
-print(M)
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+ 
+img = cv2.imread('capi.jpg')
+ 
+blur = cv2.blur(img,(3,3))
+ 
+plt.subplot(121),plt.imshow(img),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(blur),plt.title('Difuminada')
+plt.xticks([]), plt.yticks([])
+plt.show()
